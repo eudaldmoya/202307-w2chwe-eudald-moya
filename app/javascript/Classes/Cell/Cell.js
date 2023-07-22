6a373233615fd73e95f208ec360d144cbd131d28
eudaldmoya <eudald.moya.8@gmail.com>
2023-07-22 14:11:23 +0200

    Define cell properties

--- app/javascript/Classes/Cell/Cell.js
+++ app/javascript/Classes/Cell/Cell.js
@@ -1,5 +1,13 @@
 class Cell {
   isAlive;
+  Iposition;
+  Jposition;
+
+  constructor(Iposition, Jposition, isAlive = false) {
+    this.Iposition = Iposition;
+    this.Jposition = Jposition;
+    this.isAlive = isAlive;
+  }
 }
 
 export default Cell;
