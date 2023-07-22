aa2acaa6ee768a1c66e23616f9f10d51bf14cd47
eudaldmoya <eudald.moya.8@gmail.com>
2023-07-22 13:54:32 +0200

    Add too big matrix error test

--- app/javascript/Classes/generateMatrix.test.js
+++ app/javascript/Classes/generateMatrix.test.js
@@ -1,20 +1,37 @@
 import generateMatrix from "../generateMatrix";
 
 describe("Given a generateMatrix function", () => {
-  describe("When it receives 2, 2", () => {
-    test("Then it should return an array with 4 objects", () => {
-      const rows = 2;
-      const columns = 2;
+  describe("When it receives 3, 3", () => {
+    test("Then it should return an array with 9 objects", () => {
+      const rows = 3;
+      const columns = 3;
       const expectedArray = [
         { i: 1, j: 1 },
         { i: 1, j: 2 },
+        { i: 1, j: 3 },
         { i: 2, j: 1 },
         { i: 2, j: 2 },
+        { i: 2, j: 3 },
+        { i: 3, j: 1 },
+        { i: 3, j: 2 },
+        { i: 3, j: 3 },
       ];
 
       const generatedMatrix = generateMatrix(rows, columns);
 
       expect(generatedMatrix).toStrictEqual(expectedArray);
+    });
+  });
+
+  describe("When it receives 60, 60", () => {
+    test("Then it should throw error with message 'Matrix must be 50x50 or less'", () => {
+      const rows = 60;
+      const columns = 60;
+      const expectedTextError = "Matrix must be 50x50 or less";
+
+      expect(() => {
+        generateMatrix(rows, columns);
+      }).toThrow(new Error(expectedTextError));
     });
   });
 });
--- app/javascript/generateMatrix.js
+++ app/javascript/generateMatrix.js
@@ -1,4 +1,12 @@
 const generateMatrix = (rows, columns) => {
+  if (rows < 3 || columns < 3) {
+    throw new Error("Matrix must be at least 3x3");
+  }
+
+  if (rows > 50 || columns > 50) {
+    throw new Error("Matrix must be 50x50 or less");
+  }
+
   const itemsList = [];
   for (let i = 1; i <= rows; i++) {
     for (let j = 1; j <= columns; j++) {
