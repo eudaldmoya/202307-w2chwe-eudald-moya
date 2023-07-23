eaf399f6a32f0ac44ef3fa9d2ba23cda4c154a6b
eudaldmoya <eudald.moya.8@gmail.com>
2023-07-23 14:08:28 +0200

    Update Cell test to new structure

--- app/javascript/Classes/Cell/Cell.test.js
+++ app/javascript/Classes/Cell/Cell.test.js
@@ -1,14 +1,12 @@
 import Cell from "./Cell";
 
 describe("Given a class Cell", () => {
-  describe("When instanciating with 2, 2, true", () => {
-    test("Then it should return a cell {}", () => {
-      const iPosition = 2;
-      const jPosition = 3;
+  describe("When instanciating with true", () => {
+    test("Then it should return a cell {isAlive: true}", () => {
       const isAlive = true;
-      const expectedObject = { iPosition: 2, jPosition: 3, isAlive: true };
+      const expectedObject = { isAlive: true };
 
-      const instanciatedObject = new Cell(iPosition, jPosition, isAlive);
+      const instanciatedObject = new Cell(isAlive);
 
       expect(instanciatedObject).toEqual(expectedObject);
     });
